77786567f7d99f23a5df3ceb514f8ca97420e952
ChadithDissanayaka <124421386+ChadithDissanayaka@users.noreply.github.com>
2023-04-04 00:33:54 +0530

    add other ctrls

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 
-
-from src.controllers.authController import authCtrl 
+from src.controllers.taskController import taskCtrl
+from src.controllers.userController import userCtrl
+from src.controllers.authController import authCtrl
 from src.ml_modal.modalController import mlCtrl
+
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
+app.register_blueprint(userCtrl, url_prefix='/user')
+app.register_blueprint(taskCtrl, url_prefix='/task')
 app.register_blueprint(authCtrl, url_prefix='/auth')
 app.register_blueprint(mlCtrl, url_prefix='/ml')
